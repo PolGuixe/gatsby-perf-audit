fa33f686f97b635d701d38920a195021fa7d3ccd
PolGuixe <pol.guixe@gmail.com>
2019-02-03 18:39:51 +0100

    test improvements
    - added a longer jest timeout
    - added a conditional to only run the test when the server is running

--- lighthouse.test.js
+++ lighthouse.test.js
@@ -1,6 +1,27 @@
-const lighthouse = require('lighthouse');
-const chromeLauncher = require('chrome-launcher');
+const lighthouse = require('lighthouse')
+const chromeLauncher = require('chrome-launcher')
 const Table = require('cli-table')
+var net = require('net')
+var Promise = require('bluebird')
+
+function checkConnection(host, port, timeout) {
+  return new Promise(function(resolve, reject) {
+    timeout = timeout || 10000 // default of 10 seconds
+    var timer = setTimeout(function() {
+      reject('timeout')
+      socket.end()
+    }, timeout)
+    var socket = net.createConnection(port, host, function() {
+      clearTimeout(timer)
+      resolve()
+      socket.end()
+    })
+    socket.on('error', function(err) {
+      clearTimeout(timer)
+      reject(err)
+    })
+  })
+}
 
 let table = new Table()
 
@@ -12,17 +33,28 @@ const output = scores => {
 }
 
 function launchChromeAndRunLighthouse(url, opts = {}, config = null) {
-  return chromeLauncher.launch({chromeFlags: opts.chromeFlags}).then(chrome => {
-    opts.port = chrome.port;
-    return lighthouse(url, opts, config).then(results => {
-      return chrome.kill().then(() => results)
-    });
-  });
+  return chromeLauncher
+    .launch({ chromeFlags: opts.chromeFlags })
+    .then(chrome => {
+      opts.port = chrome.port
+      return lighthouse(url, opts, config).then(results => {
+        return chrome.kill().then(() => results)
+      })
+    })
 }
 
 test('performance audit', async () => {
+  checkConnection('localhost', 9000)
+    .then(() => {})
+    .catch(err => {
+      console.warn(
+        'Performance not tested due to server not being available',
+        err
+      )
+      return
+    })
   const { lhr } = await launchChromeAndRunLighthouse('http://localhost:9000')
-  
+
   const scores = Object.keys(lhr.categories).reduce((merged, category) => {
     merged[category] = lhr.categories[category].score
     return merged
@@ -34,4 +66,4 @@ test('performance audit', async () => {
   expect(scores.accessibility).toBe(1)
   expect(scores['best-practices']).toBeGreaterThanOrEqual(0.93)
   expect(scores.seo).toBe(1)
-})
+}, 20000)
